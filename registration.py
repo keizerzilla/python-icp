ea1b8385ac4f128513bcc08a6a5bb9efedf83d0c
Artur Rodrigues <artur.rodrigues26@gmail.com>
2022-06-23 19:25:34 -0300

    kmedias
    - refatorcao da documentacao
    - adicao da funcao que segmenta nuvem usando kmedias

--- registration.py
+++ registration.py
@@ -1,6 +1,7 @@
 import numpy as np
 import open3d as o3d
 from matplotlib import cm
+from sklearn.cluster import KMeans
 from sklearn.neighbors import NearestNeighbors
 
 
@@ -10,11 +11,11 @@ def load_cloud(file_path):
     
     Parâmetros
     ----------
-    file_path: Caminho completo para o arquivo com a nuvem.
+    file_path: Caminho completo para o arquivo com a nuvem
     
     Retornos
     --------
-    xyz: Matriz (n x 3) com os pontos da nuvem.
+    xyz: Matriz (n x 3) com os pontos da nuvem
     
     """
     
@@ -50,6 +51,34 @@ def draw_clouds(clouds):
     o3d.visualization.draw_geometries(draw, width=800, height=600, window_name="plot_slice")
 
 
+def cloud_cluster_kmeans(xyz, n_clusters=4):
+    """
+    Clusteriza uma nuvem de pontos usando o algoritmo k-médias.
+    
+    Parâmetros
+    ----------
+    xyz: Uma nuvem de pontos em matriz numpy de dimensão (n x 3)
+    n_clusters: número de grupos (clusters, segmentos) que devem ser calculados
+    
+    Retornos
+    --------
+    indices: vetor (n x 1) com os índices de cada ponto de xyz para cada grupo
+    
+    Notas
+    -----
+    - Para separar a matriz original da nuvem, basta indexar a matriz a partir
+      do vetor de indices de grupos. Exemplo:
+        A[indices == 0] # apenas os pontos de A do grupo 0
+        A[indices == 1] # apenas os pontos de A do grupo 1
+    
+    """
+    
+    kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(xyz)
+    indices = kmeans.labels_
+    
+    return indices
+
+
 def best_fit_transform(A, B):
     """
     Calcula a melhor transformação entre as matrizes A e B que minimiza o erro
@@ -57,13 +86,13 @@ def best_fit_transform(A, B):
     
     Parâmetros
     ----------
-    A: Matriz (n x 3) de pontos correspondentes A.
-    B: Matriz (n x 3) de pontos correspondentes B.
+    A: Matriz (n x 3) de pontos correspondentes A
+    B: Matriz (n x 3) de pontos correspondentes B
     
     Retornos
     -------
-    R: Matriz de rotação (3 x 3).
-    t: Vetor de translação (3 x 1).
+    R: Matriz de rotação (3 x 3)
+    t: Vetor de translação (3 x 1)
     
     """
     
@@ -95,14 +124,14 @@ def matching_subclouds(src, dst):
     
     Parâmetros
     ----------
-    src: Matriz (n x m) de pontos da nuvem de origem.
-    dst: Matriz (n x m) de pontos da nuvem de destino.
+    src: Matriz (n x m) de pontos da nuvem de origem
+    dst: Matriz (n x m) de pontos da nuvem de destino
     
     Retornos
     -------
-    src_match: Sub-nuvem de origem de tamanho (min_n x 3).
-    dst_match: Sub-nuvem de destino de tamanho (min_n x 3).
-    dist: Vetor com (min_n) distâncias entre os pontos correspondentes.
+    src_match: Sub-nuvem de origem de tamanho (min_n x 3)
+    dst_match: Sub-nuvem de destino de tamanho (min_n x 3)
+    dist: Vetor com (min_n) distâncias entre os pontos correspondentes
     
     """
     
@@ -128,11 +157,11 @@ def rmse(distances):
     
     Parâmetros
     ----------
-    distances: Vetor de distâncias.
+    distances: Vetor de distâncias
     
     Retornos
     --------
-    rmse: Erro quadrático médio.
+    rmse: Erro quadrático médio
     """
     
     rmse = np.sqrt(np.mean(distances**2))
@@ -148,18 +177,18 @@ def icp(A, B, max_iterations=20, min_error=0.01, verbose=True):
     
     Parâmetros
     ----------
-    A: Nuvem de pontos 3D de origem com n pontos, ou seja, matriz de dimensão (n x 3).
-    B: Nuvem de pontos 3D de destino com m pontos, ou seja, matriz de dimensão (m x 3).
-    max_iterations: Critério de parada 1: máximo número de iterações.
-    min_error: Critério de parada 2: erro quadrático médido mínimo de uma iteração.
-    verbose: Se True, mostra mensagem de acompanhamento na saída padrão.
+    A: Nuvem de pontos 3D de origem com n pontos, ou seja, matriz de dimensão (n x 3)
+    B: Nuvem de pontos 3D de destino com m pontos, ou seja, matriz de dimensão (m x 3)
+    max_iterations: Critério de parada 1: máximo número de iterações
+    min_error: Critério de parada 2: erro quadrático médido mínimo de uma iteração
+    verbose: Se True, mostra mensagem de acompanhamento na saída padrão
     
     Retornos
     -------
-    R: Matriz de rotação (3 x 3) que melhor corrige rotação entre A com B.
-    t: Vetor de translação (3 x 1) que melhor corrige translação entre A e B.
-    i: Número de iterações necessárias até final do algoritmo.
-    error: Erro quadrá médio da última iteração do algoritmo.
+    R: Matriz de rotação (3 x 3) que melhor corrige rotação entre A com B
+    t: Vetor de translação (3 x 1) que melhor corrige translação entre A e B
+    i: Número de iterações necessárias até final do algoritmo
+    error: Erro quadrá médio da última iteração do algoritmo
     
     """
     
